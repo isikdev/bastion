575952b3732fedcdba1b1761b42ba0599b0952bc
isikdev <isikjandev@gmail.com>
2023-12-14 17:37:18 +0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ $('.count').each(function () {
 let preloader = $(".preloader");
 let navbar = $(".navbar");
 $(window).on('load', function () {
-    preloader.fadeOut(100000);
+    preloader.fadeOut(1000);
 });  
 let btn = $(".menu__mob")
 
