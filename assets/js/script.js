30b552013e76124f2403a782dc004e285ec2601d
Iskandar <isikjandev@gmail.com>
2023-12-14 16:26:23 +0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $('.count').each(function () {
     $(this).prop('Counter', 0).animate({
         Counter: parseFloat($(this).text())
     }, {
-        duration: 2500,
+        duration: 500,
         easing: 'swing',
         step: function (now) {
             $(this).text(Math.ceil(now));
